66fd91a0ba8731673ef74cc296ee0b76aad76c13
Trevahr Robertson <trevahr@yahoo.com>
2019-05-30 22:28:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  pigeon_list = {}
+  
+    data.each do |attributes, types|
+      
+      types.each do |adjective, name_array|
+    	
+    	name_array.each do |name|
+
+
+          if pigeon_list[name].nil?
+            
+            	pigeon_list[name] = {"#{attributes}": ["#{adjective}"]}
+          
+          elsif 
+          		pigeon_list[name][attributes].nil?
+
+          		pigeon_list[name][attributes]=["#{adjective}"]
+
+          elsif 
+          		data[attributes][adjective].include?(name)
+          	   pigeon_list[name][attributes] << "#{adjective}"
+    	end
+      end
+    end
+  end
+  pigeon_list
 end
